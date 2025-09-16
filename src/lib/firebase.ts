580852b59d57618494f0f673090897539b1f1a2d
Amaan Ali <amaanali3521@gmail.com>
2025-09-16 22:07:04 +0530

    env added

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeFVJFhM9HaxPQDOal12NjQ951Uhf1Zgc",
-  authDomain: "video-proctoring-1e65c.firebaseapp.com",
-  projectId: "video-proctoring-1e65c",
-  storageBucket: "video-proctoring-1e65c.firebasestorage.app",
-  messagingSenderId: "18905375112",
-  appId: "1:18905375112:web:fa0122dd4489d863c99a3d",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -17,8 +17,3 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 export default app;
-
-
-
-
-
